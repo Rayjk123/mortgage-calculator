f043f3dadb73575b0471d665336f5f5a292e1567
Branden Lee <branden.lee@coxautoinc.com>
2019-12-17 14:58:13 -0500

    Adding ESLint and fixing things that don't abide to eslint

--- app/components/FormLine.jsx
+++ app/components/FormLine.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { StyleSheet, Text, View } from "react-native";
-import NumberTextInput from "./NumberTextInput.js";
-import { inputStyle } from "../Styles/style";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text, View } from 'react-native';
+import NumberTextInput from './NumberTextInput';
+import { inputStyle } from '../Styles/style';
 
 const FormLine = ({ label, value, valueUpdateCB }) => {
-  //console.log('VALUE UPDATE CB IN FORM LINE ' + valueUpdateCB);
-  //console.log('VALUE UPDATE CB IN FORM LINE ' + typeof valueUpdateCB);
+  // console.log('VALUE UPDATE CB IN FORM LINE ' + valueUpdateCB);
+  // console.log('VALUE UPDATE CB IN FORM LINE ' + typeof valueUpdateCB);
   const removeNonNumericValues = (label, value) => {
     if (!value) {
       value = 0;
@@ -14,8 +14,8 @@ const FormLine = ({ label, value, valueUpdateCB }) => {
       const regexNotNumberOrDot = /[^0-9.]/g;
       const regexLeadingNumber = /^0+/;
 
-      value = value.replace(regexNotNumberOrDot, "");
-      value = value.replace(regexLeadingNumber, "");
+      value = value.replace(regexNotNumberOrDot, '');
+      value = value.replace(regexLeadingNumber, '');
     }
     valueUpdateCB(label, value);
   };
@@ -35,7 +35,7 @@ const FormLine = ({ label, value, valueUpdateCB }) => {
 FormLine.propTypes = {
   label: PropTypes.string.isRequired,
   value: PropTypes.number.isRequired,
-  valueUpdateCB: PropTypes.func.isRequired
+  valueUpdateCB: PropTypes.func.isRequired,
 };
 
 export default FormLine;
--- app/constants/constants.js
+++ app/constants/constants.js
@@ -1,7 +1,7 @@
-export const FORM_UPDATE = "FORM_UPDATE";
+export const FORM_UPDATE = 'FORM_UPDATE';
 
-export const MORTGAGE_AMOUNT = "Mortgage Amount";
-export const MORTGAGE_INTEREST = "Mortgage Interest";
-export const MORTGAGE_YEARS = "Mortgage Years";
+export const MORTGAGE_AMOUNT = 'Mortgage Amount';
+export const MORTGAGE_INTEREST = 'Mortgage Interest';
+export const MORTGAGE_YEARS = 'Mortgage Years';
 
-export const LOL = "~~~~~~~~~~~~~~~~~~~~~~~~~~";
+export const LOL = '~~~~~~~~~~~~~~~~~~~~~~~~~~';
