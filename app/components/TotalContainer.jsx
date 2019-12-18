9f4b810936b0cf80ebd3a8b520e9b302b8d5d958
daniel0226 <daniel1996k@icloud.com>
2019-12-17 22:45:03 -0500

    reformat 2

--- app/components/TotalContainer.jsx
+++ app/components/TotalContainer.jsx
@@ -4,17 +4,17 @@ import PropTypes from 'prop-types';
 import { View, Text } from 'react-native';
 
 const TotalContainer = ({ label, value }) => {
-	return (
-		<View style={totalStyle.container}>
-			<Text style={totalStyle.textLabel}>{label}</Text>
-			<Text style={totalStyle.numFont}>{value.toString()}</Text>
-		</View>
-	);
+  return (
+    <View style={totalStyle.container}>
+      <Text style={totalStyle.textLabel}>{label}</Text>
+      <Text style={totalStyle.numFont}>{value.toString()}</Text>
+    </View>
+  );
 };
 
 TotalContainer.propTypes = {
-	label: PropTypes.string.isRequired,
-	value: PropTypes.number.isRequired,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
 };
 
 export default TotalContainer;
