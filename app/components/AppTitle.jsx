582677fefc2f8280f2c86d380d539255e848e0ce
daniel0226 <daniel1996k@icloud.com>
2019-12-21 01:31:24 -0500

    eslint is cancer

--- app/components/AppTitle.jsx
+++ app/components/AppTitle.jsx
@@ -3,12 +3,10 @@ import { View, Text } from 'react-native';
 import { bodyStyle } from '../Styles/style';
 import { MORTGAGE_TITLE } from '../constants/constants';
 
-const ApplicationTitle = () => {
-  return (
-    <View style={bodyStyle.TitleContainer}>
-      <Text style={bodyStyle.Title}>{MORTGAGE_TITLE}</Text>
-    </View>
-  );
-};
+const ApplicationTitle = () => (
+  <View style={bodyStyle.TitleContainer}>
+    <Text style={bodyStyle.Title}>{MORTGAGE_TITLE}</Text>
+  </View>
+);
 
 export default ApplicationTitle;
